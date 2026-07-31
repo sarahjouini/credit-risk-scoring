31ca27e1905a8fa1354eedb3a921a3c3754fded3
sarahjouini <sarajouini612@gmail.com>
2026-07-31 20:02:26 +0000

    scoring completo: punti debiti + ritardi + rischio finale

--- analisi.py
+++ analisi.py
@@ -4,18 +4,16 @@ df = pd.read_csv("aziende.csv")
 
 df["rapporto_debiti"] = df["debiti"] / df["fatturato"]
 
-print(df[["nome_azienda", "debiti", "fatturato", "rapporto_debiti"]].head(10))
-def assegna_rischio(rapporto):
+
+def punti_debiti(rapporto):
     if rapporto < 0.5:
-        return "Basso"
+        return 0
     elif rapporto < 1:
-        return "Medio"
+        return 1
     else:
-        return "Alto"
+        return 2
 
-df["rischio"] = df["rapporto_debiti"].apply(assegna_rischio)
 
-print(df[["nome_azienda", "rapporto_debiti", "rischio"]].head(10))
 def punti_ritardo(giorni):
     if giorni < 30:
         return 0
@@ -23,6 +21,22 @@ def punti_ritardo(giorni):
         return 1
     else:
         return 2
+
+
+def rischio_finale(punti):
+    if punti <= 1:
+        return "Basso"
+    elif punti == 2:
+        return "Medio"
+    else:
+        return "Alto"
+
+
+df["punti_debiti"] = df["rapporto_debiti"].apply(punti_debiti)
 df["punti_ritardo"] = df["ritardo_medio_pagamenti"].apply(punti_ritardo)
 
-print(df[["nome_azienda", "ritardo_medio_pagamenti", "punti_ritardo"]].head(10))
+df["punti_totali"] = df["punti_debiti"] + df["punti_ritardo"]
+
+df["rischio_finale"] = df["punti_totali"].apply(rischio_finale)
+
+print(df[["nome_azienda", "punti_debiti", "punti_ritardo", "punti_totali", "rischio_finale"]].head(10))
